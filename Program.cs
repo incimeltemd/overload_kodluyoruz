6f37260a56557e31afbb3a5ae6e6c1e3ad3670a2
incimeltemd <incimeltemdaglaroglu@gmail.com>
2023-02-21 16:12:46 +0300

    ödev tamam

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        //metot overloading
+
+        Metotlar ornek = new Metotlar();
+        ornek.Topla(5, 7, 9, out int toplam);
+        Console.WriteLine(toplam);
+    }
+       
+}
+
+
+
+class Metotlar
+{
+
+    public void Topla (int a, int b, out int toplam)
+    {
+        toplam = a + b;
+    }
+    public void Topla(int x, int y, int z,out int toplam)
+    {
+        toplam=x+y+z;
+    }
+}
